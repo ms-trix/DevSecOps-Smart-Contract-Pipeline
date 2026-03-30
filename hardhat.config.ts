3faeadad75a3c296422e1faf589de2a30a604f56
Matas <mskrebe@gmail.com>
2026-03-30 23:54:25 +0300

    feat: add Github actions security pipeline

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from "hardhat/config";
+import type { HardhatUserConfig } from "hardhat/types/config";
 import "@nomicfoundation/hardhat-ethers";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-export default defineConfig({
+const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.28",
   },
-});
+  networks: {
+    sepolia: {
+      type: "http",
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.METAMASK_API ? [process.env.METAMASK_API] : [],
+    },
+  },
+};
+
+export default config;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,16 @@ import { ethers } from "ethers";
 import hre from "hardhat";
 
 async function main() {
-    const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+    const networkConfig = hre.network.config;
+    const url = networkConfig.url ?? "http://127.0.0.1:8545";
+    const provider = new ethers.JsonRpcProvider(url);
 
-    const deployer = await provider.getSigner(0);
-    const user = await provider.getSigner(1);
+    let deployer;
+    if (process.env.METAMASK_API) {
+        deployer = new ethers.Wallet(process.env.METAMASK_API, provider);
+    } else {
+        deployer = await provider.getSigner(0);
+    }
 
     console.log("Deploying with:", await deployer.getAddress());
 
@@ -21,41 +27,6 @@ async function main() {
 
     const bankAddress = await bank.getAddress();
     console.log("Bank deployed to:", bankAddress);
-
-    await bank.connect(user).deposit({
-        value: ethers.parseEther("10"),
-    });
-
-    console.log("Bank funded with 10 ETH");
-
-    const AttackerArtifact = await hre.artifacts.readArtifact("Attacker");
-    const AttackerFactory = new ethers.ContractFactory(
-        AttackerArtifact.abi,
-        AttackerArtifact.bytecode,
-        deployer
-    );
-
-    const attacker = await AttackerFactory.deploy(bankAddress);
-    await attacker.waitForDeployment();
-
-    const attackerAddress = await attacker.getAddress();
-    console.log("Attacker deployed to:", attackerAddress);
-
-
-
-    const tx = await attacker.connect(deployer).attack({
-        value: ethers.parseEther("1"),
-        gasLimit: 3000000
-    });
-    await tx.wait();
-
-    console.log("Attack executed");
-
-    const bankBalance = await provider.getBalance(bankAddress);
-    const attackerBalance = await provider.getBalance(attackerAddress);
-
-    console.log("Bank balance:", ethers.formatEther(bankBalance));
-    console.log("Attacker balance:", ethers.formatEther(attackerBalance));
 }
 
 main().catch((error) => {
